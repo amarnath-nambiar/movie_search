d7dd8b1aa20281ec6833c9f76d9eb82b733f276d
Joshi <amarnathjoshy@gmail.com>
2020-06-13 22:41:40 +0530

    Show movie details

--- src/searchMovies.jsx
+++ src/searchMovies.jsx
@@ -13,11 +13,12 @@ export default function SearchMovie(){
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
     }
+
     return (
       <>
         <form className="form" onSubmit={searchMovies}>
@@ -35,10 +36,16 @@ export default function SearchMovie(){
                             src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
                             alt={movie.title + ' poster'}
                             />
+                        <div className="card--content">
+                            <h3 className="card--title">{movie.title}</h3>
+                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                            <p><small>RATING: {movie.vote_average}</small></p>
+                            <p className="card--desc">{movie.overview}</p>
+                        </div>
 
                     </div>
                 ))}
             </div>
-      </>      
+      </>
     )
 }
